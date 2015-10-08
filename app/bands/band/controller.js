51292b38f755dcc374c8e1137c9aefcfea8852de
Balint Erdi <balint.erdi@gmail.com>
2015-10-08 15:58:09 +0200

    Show error on name upon submit

--- app/bands/band/controller.js
+++ app/bands/band/controller.js
@@ -1,6 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  hasValidName: Ember.computed.notEmpty('model.name'),
+  hasErrors: Ember.computed.not('hasValidName'),
+
+  setupErrors: Ember.on('init', function() {
+    this.set('errors', Ember.Object.create());
+  }),
+
+  validate() {
+    this.set('errors.name', this.get('hasValidName') ? null : "Name is required.");
+  },
+
   actions: {
     updateRating(params) {
       const { item: band, rating } = params;
@@ -8,6 +19,11 @@ export default Ember.Controller.extend({
     },
 
     saveBand() {
+      this.validate();
+      if (this.get('hasErrors')) {
+        return;
+      }
+
       const band = this.get('model');
       return band.save().then(() => {
         console.log("Band is saved");
--- app/components/band-form/component.js
+++ app/components/band-form/component.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'form',
+  band: null,
+  errors: null,
   "on-star-click": null,
 
   actions: {
