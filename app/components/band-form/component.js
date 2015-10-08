0205deb6b8b7607e60870dea68c66207cff9f1ab
Balint Erdi <balint.erdi@gmail.com>
2015-10-08 17:23:41 +0200

    Move the button onto the band-form

--- app/components/band-form/component.js
+++ app/components/band-form/component.js
@@ -3,4 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'form',
   "on-star-click": null,
+
+  actions: {
+    saveBand() {
+      this.attrs['on-save']();
+    }
+  }
 });
