daf032b752a9a08af8ea8bc10fedf6fa8ab3fe34
Balint Erdi <balint.erdi@gmail.com>
2015-10-08 17:52:48 +0200

    Add possibility of resetting form and moving validation onto it
    * Splitting up the two-way binding into one-way bindings and actions made it possible to only copy the changed values over to the band if it is valid
    * It also made possible to reset changed values as we still have the original ones on the band object

--- app/bands/band/controller.js
+++ app/bands/band/controller.js
@@ -1,29 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  hasValidName: Ember.computed.notEmpty('model.name'),
-  hasErrors: Ember.computed.not('hasValidName'),
-
-  setupErrors: Ember.on('init', function() {
-    this.set('errors', Ember.Object.create());
-  }),
-
-  validate() {
-    this.set('errors.name', this.get('hasValidName') ? null : "Name is required.");
-  },
-
   actions: {
-    updateBandProperty(property, value) {
-      this.get('model').set(property, value);
-    },
-
-    saveBand() {
-      this.validate();
-      if (this.get('hasErrors')) {
-        return;
-      }
-
+    saveBand(properties) {
       const band = this.get('model');
+      band.setProperties(properties);
+
       return band.save().then(() => {
         console.log("Band is saved");
       });
--- app/components/band-form/component.js
+++ app/components/band-form/component.js
@@ -3,23 +3,61 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'form',
   band: null,
-  errors: null,
-  "on-update": null,
+  "on-save": null,
+
+  name:    null,
+  year:    null,
+  rating:  null,
+  errors:  null,
+
+  hasValidName: Ember.computed.notEmpty('name'),
+  hasErrors: Ember.computed.not('hasValidName'),
+
+  setupErrors: Ember.on('init', function() {
+    this.set('errors', Ember.Object.create());
+  }),
+
+  validate() {
+    this.set('errors.name', this.get('hasValidName') ? null : "Name is required.");
+  },
+
+  resetOnInit: Ember.on('init', function() {
+    this.resetFromBand();
+  }),
+
+  resetFromBand() {
+    ['name', 'year', 'rating'].forEach((field) => {
+      const valueInBand = this.get('band').get(field);
+      this.set(field, valueInBand);
+    });
+  },
+
+  updateField(field, value) {
+    this.set(field, value);
+  },
 
   actions: {
     saveBand() {
-      this.attrs['on-save']();
+      this.validate();
+      if (this.get('hasErrors')) {
+        return;
+      }
+
+      return this.attrs['on-save'](this.getProperties(['name', 'year', 'rating']));
     },
 
     nameDidChange(value) {
-      this.attrs['on-update']('name', value);
+      this.updateField('name', value);
     },
-    titleDidChange(value) {
-      this.attrs['on-update']('title', value);
+    yearDidChange(value) {
+      this.updateField('year', value);
     },
     ratingDidChange(params) {
       const { rating } = params;
-      this.attrs['on-update']('rating', rating);
+      this.updateField('rating', rating);
     },
+    reset() {
+      this.resetFromBand();
+    }
   }
 });
